31cf6cecc34915a379ce55271a5cfb2aa83b4285
Tim Licata <tim@timothylicata.com>
2014-11-06 21:09:55 -0800

    can add a new animal to a zoo

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -2,4 +2,16 @@ class AnimalsController < ApplicationController
   def index
     @zoo = Zoo.find(params[:zoo_id])
   end
+
+  def new
+    @zoo = Zoo.find(params[:zoo_id])
+    @animal = Animal.new
+  end
+
+  def create
+    @zoo = Zoo.find(params[:zoo_id])
+    new_animal = params.require(:animal).permit(:name, :photo)
+    @zoo.animals.create(new_animal)
+    redirect_to @zoo
+  end
 end
